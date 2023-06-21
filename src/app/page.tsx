60b5182e640dbccaa9c8a5d968b6d5f23f5825cb
glennanj1 <glennan20@gmail.com>
2023-06-21 11:50:42 -0400

    Dynamic image rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -16,27 +17,44 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{' '}
-            <Image
-              src="/techbridgelogo.png"
-              alt="Vercel Logo"
-              className=""
-              width={200}
-              height={100}
-              priority
-            />
+        
+            <picture>
+              <source
+                srcSet="/techbridgelogo.png"
+                media="(prefers-color-scheme: dark)"
+              />
+              <img
+                src="/techBridgeDarkLogo.png"
+                alt="techbridge logo"
+                width={200}
+                height={100}
+                
+              />
+            </picture>
           </a>
         </div>
       </div>
 
       <div className="relative flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
+        <picture>
+          <source
+            srcSet="/techbridgelogo.png"
+            media="(prefers-color-scheme: dark)"
+          />
+          <img
+            className="md:block hidden relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            src="/techBridgeDarkLogo.png"
+            alt="techbridge logo"
+          />
+        </picture>
+        {/* <Image
           className="md:block hidden relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/techbridgelogo.png"
           alt="Next.js Logo"
           width={900}
           height={180}
           priority
-        />
+        /> */}
         <Image
           className="md:hidden animate-pulse relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/smallLogo.png"
@@ -45,10 +63,10 @@ export default function Home() {
           height={200}
           priority
         />
-        <p className="md:hidden pb-10 text-2xl text-center text-gray-300 grid">
+        <p className="md:hidden pb-10 text-2xl text-center text-gray-300 grid font-mono font-bold">
           TechBridge Consulting LLC
         </p>
-        <p className="pb-10 text-center grid mb-5 md:text-xl text-blue-600 md:underline-offset-2">
+        <p className="pb-10 text-center grid mb-5 md:text-xl text-blue-600 md:underline-offset-2 font-mono">
            Bridging the gap between people and technology
         </p>
       </div>
